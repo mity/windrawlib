fcc0131441a8ba473e69b89a0c11ae0a30d3a750
Martin Mitas <mity@morous.org>
2016-09-17 21:01:01 +0200

    src/misc.h: Hacks for compatibility with MSVC 6.0.

--- src/misc.h
+++ src/misc.h
@@ -38,11 +38,17 @@
  ***  Debug Logging  ***
  ***********************/
 
+#if !defined _MSC_VER || (defined _MSC_VER && _MSC_VER > 1200)
+#define no_log(...)     do { } while(0)
+#else
+static void no_log(const char* fmt, ...) {}
+#endif
+
 #ifdef DEBUG
     void wd_log(const char* fmt, ...);
-    #define WD_TRACE(...)       do { wd_log(__VA_ARGS__); } while(0)
+    #define WD_TRACE    wd_log
 #else
-    #define WD_TRACE(...)       do { } while(0)
+    #define WD_TRACE    no_log
 #endif
 
 #define WD_TRACE_ERR_(msg, err)          WD_TRACE(msg " [%lu]", (err))
@@ -77,6 +83,19 @@ HMODULE wd_load_system_dll(const TCHAR* dll_name);
      * However it understands "__inline" */
     #ifndef __cplusplus
         #define inline __inline
+    #endif
+
+    #if _MSC_VER <= 1200
+        #define _malloca(_size)     HeapAlloc(GetProcessHeap(), 0, _size)
+        #define _freea(_ptr)        HeapFree(GetProcessHeap(), 0, _ptr)
+
+        static inline float floorf(float x) { return (float)floor((double)x); }
+        static inline float ceilf(float x) { return (float)ceil((double)x); }
+        static inline float powf(float x, float y) { return (float)pow((double)x, (double)y); }
+        static inline float cosf(float x) { return (float)cos((double)x); }
+        static inline float sinf(float x) { return (float)sin((double)x); }
+        static inline float atan2f(float x, float y) { return (float)atan2((double)x, (double)y); }
+        static inline float sqrtf(float x) { return (float)sqrt((double)x); }
     #endif
 #endif
 
