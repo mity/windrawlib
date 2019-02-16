b388b2df09b5301ca411d3ea15aebe1e7519158e
Martin Mitas <mity@morous.org>
2019-02-17 00:19:31 +0100

    init.c: Refactor.
    We need to redo the initialization because WinDrawLib interface modules
    do not match 1:1 to the implementation modules, from now on.
    
    In particular, with the D2D1 back-end, we use DWrite both for the string
    API and (new not yet implemented) advanced text API.
    
    The reason for disntinguishing string and text API is that later, we
    shall likely implement the text API also for GDI+ back-end using
    Uniscribe, but the simple string API does not need to load it.

--- src/init.c
+++ src/init.c
@@ -1,6 +1,6 @@
 /*
  * WinDrawLib
- * Copyright (c) 2016 Martin Mitas
+ * Copyright (c) 2016-2019 Martin Mitas
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -45,7 +45,7 @@ wdPreInitialize(void (*fnLock)(void), void (*fnUnlock)(void), DWORD dwFlags)
 
 
 static int
-wd_init_core_api(void)
+core_init(void)
 {
     if(!(wd_preinit_flags & WD_DISABLE_D2D)) {
         if(d2d_init() == 0)
@@ -61,7 +61,7 @@ wd_init_core_api(void)
 }
 
 static void
-wd_fini_core_api(void)
+core_fini(void)
 {
     if(d2d_enabled())
         d2d_fini();
@@ -69,156 +69,126 @@ wd_fini_core_api(void)
         gdix_fini();
 }
 
-static int
-wd_init_image_api(void)
-{
-    if(d2d_enabled()) {
-        return wic_init();
-    } else {
-        /* noop */
-        return 0;
-    }
-}
+static UINT core_counter = 0;
+static UINT wic_counter = 0;
+static UINT dwrite_counter = 0;
 
-static void
-wd_fini_image_api(void)
-{
-    if(d2d_enabled()) {
-        wic_fini();
-    } else {
-        /* noop */
-    }
-}
+#define WD_MOD_WIC          0x0001
+#define WD_MOD_DWRITE       0x0002
 
-static int
-wd_init_string_api(void)
-{
-    if(d2d_enabled()) {
-        return dwrite_init();
-    } else {
-        /* noop */
-        return 0;
-    }
-}
-
-static void
-wd_fini_string_api(void)
-{
-    if(d2d_enabled()) {
-        dwrite_fini();
-    } else {
-        /* noop */
-    }
-}
-
-
-static const struct {
-    int (*fn_init)(void);
-    void (*fn_fini)(void);
-} wd_modules[] = {
-    { wd_init_core_api, wd_fini_core_api },
-    { wd_init_image_api, wd_fini_image_api },
-    { wd_init_string_api, wd_fini_string_api }
-};
-
-#define WD_MOD_COUNT    (sizeof(wd_modules) / sizeof(wd_modules[0]))
-
-#define WD_MOD_COREAPI          0
-#define WD_MOD_IMAGEAPI         1
-#define WD_MOD_STRINGAPI        2
-
-static UINT wd_init_counter[WD_MOD_COUNT] = { 0 };
-
+static void wdTerminateImpl(DWORD dwModMask);
 
 BOOL
 wdInitialize(DWORD dwFlags)
 {
-    BOOL want_init[WD_MOD_COUNT];
-    int i;
-
-    want_init[WD_MOD_COREAPI] = TRUE;
-    want_init[WD_MOD_IMAGEAPI] = (dwFlags & WD_INIT_IMAGEAPI);
-    want_init[WD_MOD_STRINGAPI] = (dwFlags & WD_INIT_STRINGAPI);
+    int err = -1;
+    DWORD undo = 0;
 
     wd_lock();
 
-    for(i = 0; i < WD_MOD_COUNT; i++) {
-        if(!want_init[i])
-            continue;
+    /* Core module */
+    if(core_counter == 0) {
+        if(core_init() != 0)
+            goto out;
+    }
+    core_counter++;
 
-        wd_init_counter[i]++;
-        if(wd_init_counter[i] > 0) {
-            if(wd_modules[i].fn_init() != 0)
-                goto fail;
+    /* WIC module */
+    if(d2d_enabled()  &&  (dwFlags & WD_INIT_IMAGEAPI)) {
+        if(wic_counter == 0) {
+            if(wic_init() != 0)
+                goto out;
         }
+
+        wic_counter++;
+        undo |= WD_MOD_WIC;
     }
 
-    wd_unlock();
-    return TRUE;
-
-fail:
-    /* Undo initializations from successful iterations. */
-    while(--i >= 0) {
-        if(want_init[i]) {
-            wd_init_counter[i]--;
-            if(wd_init_counter[i] == 0)
-                wd_modules[i].fn_fini();
+    /* DWrite module */
+    if(d2d_enabled()  &&  (dwFlags & (WD_INIT_STRINGAPI | WD_INIT_TEXTAPI))) {
+        if(dwrite_counter == 0) {
+            if(dwrite_init() != 0)
+                goto out;
         }
+
+        dwrite_counter++;
+        undo |= WD_MOD_DWRITE;
     }
 
+    if(gdix_enabled()  &&  (dwFlags & WD_INIT_TEXTAPI)) {
+        WD_TRACE("wdInitialize: WD_INIT_TEXTAPI for GDI+ back-end is not (yet?) implemented.");
+        goto out;
+    }
+
+    /* Success. */
+    err = 0;
+
+out:
+    /* In case of an error, undo what we have done. */
+    if(err != 0  &&  undo != 0)
+        wdTerminateImpl(undo);
+
     wd_unlock();
-    return FALSE;
+
+    return (err == 0);
+}
+
+static void
+wdTerminateImpl(DWORD dwModMask)
+{
+    /* Handle the optional modules. */
+    if((dwModMask & WD_MOD_WIC)  &&  --wic_counter == 0)
+        wic_fini();
+
+    if((dwModMask & WD_MOD_DWRITE)  &&  --dwrite_counter == 0)
+        dwrite_fini();
+
+    /* Handle the core module.
+     *
+     * Note that if the core module counter drops to zero, we make sure no
+     * optional module survives. This should only happen when  that caller
+     * forgot to pass some flags into wdTerminate() so it does not match
+     * those passed previously into wdInitialize().
+     */
+    if(--core_counter == 0) {
+        if(wic_counter > 0) {
+            WD_TRACE("wdTerminate: Forcefully terminating WIC module.");
+            wic_fini();
+            wic_counter = 0;
+        }
+        if(dwrite_counter > 0) {
+            WD_TRACE("wdTerminate: Forcefully terminating DWrite module.");
+            dwrite_fini();
+            dwrite_counter = 0;
+        }
+
+        core_fini();
+    }
 }
 
 void
 wdTerminate(DWORD dwFlags)
 {
-    BOOL want_fini[WD_MOD_COUNT];
-    int i;
+    DWORD dwModMask = 0;
 
-    want_fini[WD_MOD_COREAPI] = TRUE;
-    want_fini[WD_MOD_IMAGEAPI] = (dwFlags & WD_INIT_IMAGEAPI);
-    want_fini[WD_MOD_STRINGAPI] = (dwFlags & WD_INIT_STRINGAPI);
+    if(d2d_enabled()  &&  (dwFlags & WD_INIT_IMAGEAPI))
+        dwModMask |= WD_MOD_WIC;
+    if(d2d_enabled()  &&  (dwFlags & (WD_INIT_STRINGAPI | WD_INIT_TEXTAPI)))
+        dwModMask |= WD_MOD_DWRITE;
 
     wd_lock();
-
-    for(i = WD_MOD_COUNT-1; i >= 0; i--) {
-        if(!want_fini[i])
-            continue;
-
-        wd_init_counter[i]--;
-        if(wd_init_counter[i] == 0)
-            wd_modules[i].fn_fini();
-    }
-
-    /* If core module counter has dropped to zero, caller likely forgot to
-     * terminate some optional module (i.e. mismatching flags for wdTerminate()
-     * somewhere. So lets kill all those modules forcefully now anyway even
-     * though well behaving applications should never do that...
-     */
-    if(wd_init_counter[WD_MOD_COREAPI] == 0) {
-        for(i = WD_MOD_COUNT-1; i >= 0; i--) {
-            if(wd_init_counter[i] > 0) {
-                WD_TRACE("wdTerminate: Forcefully terminating module %d.", i);
-                wd_modules[i].fn_fini();
-                wd_init_counter[i] = 0;
-            }
-        }
-    }
-
+    wdTerminateImpl(dwModMask);
     wd_unlock();
 }
 
 int
 wdBackend(void)
 {
-    if(d2d_enabled()) {
+    if(d2d_enabled())
         return WD_BACKEND_D2D;
-    } 
     
-    if(gdix_enabled()) {
+    if(gdix_enabled())
         return WD_BACKEND_GDIPLUS;
-    }
     
-  return -1;
+    return -1;
 }
